75cc569337bf7cb08a6b4e7df0d3b862d19f7778
Guilherme Leite <guilherme.soares270@gmail.com>
2020-02-07 19:58:33 -0300

    Directory structure fix

---
+++ src/components/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Router from "../router/Router.svelte";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+    background: yellow;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<!-- <main>
+  Router
+</main> -->
+
+<Router />
---
+++ src/components/Tab.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let tabs;
+
+  let selected = 0;
+
+  const changeSelected = id => {
+    selected = id;
+    console.log(`selected: ${selected}`);
+  };
+</script>
+
+<style>
+  .tab-container {
+    background: grey;
+  }
+  .tab-item {
+    background: tomato;
+    width: 10%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .tab-header {
+    display: flex;
+    width: 100%;
+  }
+</style>
+
+<div class="tab-container">
+  <div class="tab-header">
+    {#each tabs as tab (tab.id)}
+      <div
+        class="tab-item"
+        id={tab.id - 1}
+        on:click={() => changeSelected(tab.id - 1)}>
+        tab
+      </div>
+    {/each}
+  </div>
+  <div class="tab-content">{tabs[selected].id}</div>
+
+  <!-- {#each tabs as tab }
+    <div class="tab">{tab.id}</div>
+  {/each} -->
+</div>
