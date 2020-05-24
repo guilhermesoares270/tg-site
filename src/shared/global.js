efff6af10960ef1084f1744a5b792bf7a5c92b47
Guilherme Leite <guilherme.soares270@gmail.com>
2020-05-24 12:07:33 -0300

    global

--- src/shared/global.js
+++ src/shared/global.js
@@ -1,6 +1,5 @@
-// import { useSelector } from 'react-redux';
-
-export const GLOBAL_API_URL = 'http://127.0.0.1:3333/api/v1';
+// export const GLOBAL_API_URL = 'http://127.0.0.1:3333/api/v1';
+export const GLOBAL_API_URL = 'https://tg-server270.herokuapp.com/api/v1';
 export const GLOBAL_WINDOW_TITLE = 'TG | SERVER';
 export const ROUTES_PREFIX = () => {
   const cnpj = localStorage.getItem('cnpj');
